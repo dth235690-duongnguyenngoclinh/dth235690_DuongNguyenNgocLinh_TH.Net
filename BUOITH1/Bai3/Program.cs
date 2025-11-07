59338c186f73efb45e3893bb64269293bc41df3d
dth235690-duongnguyenngoclinh <linh_dth235690@stdent.agu.edu.vn>
2025-11-08 05:15:49 +0700

    chuong1

---
+++ BUOITH1/Bai3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace Bai3
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int n;
+            GiaiThuaN gtn = new GiaiThuaN();
+            Console.Write("Nhap so n = ");
+            n = gtn.NhapMotSo();
+            Console.WriteLine("Gai thua cua {0} la {1}", n, gtn.TinhGiaiThuaN(n));
+            Console.ReadLine();
+        }
+    }
+}
